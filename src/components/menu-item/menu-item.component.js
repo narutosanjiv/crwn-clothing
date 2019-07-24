64a2298f5d275476489bcc5dc4b08beeb1ea9a32
Sanjiv Jha <narutosanjiv@gmail.com>
2019-07-25 01:37:15 +0530

    Added Style for image transformation

--- src/components/menu-item/menu-item.component.js
+++ src/components/menu-item/menu-item.component.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './menu-item.styles.scss'
 const MenuItem = ({title, imageUrl, size}) => (
-    <div style={{ backgroundImage: `url(${imageUrl})`}} 
-        className={`menu-item ${size}`} >
+    <div className={`menu-item ${size}`} >
+        <div style={{ backgroundImage: `url(${imageUrl})`}} 
+        className='background-image' ></div>   
         <div className='content'>
             <h1 className='title'>
                 {title.toUpperCase()} 
